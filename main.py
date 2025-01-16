93d7c7b52dc21e8e0f8ee7ff3f4ed8f9d51298f8
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-01-16 11:34:52 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+
+# Create welcome function
+def welcome():
+    pass
+
+# Create function to clear the screen
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+# Create add function
+def add():
+    pass
+
+# Create check out function
+def check_out():
+    pass
+
+# Create check in function
+def check_in():
+    pass
+
+# Create function to display the books in the library catalog
+def list_books():
+    pass
+
+# Create exit function
+def exit_pro():
+    pass
