de8fb833df71f0dc0eb91958df2d1f825cdd1d5a
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-01-16 12:23:40 +0300

    write code in the welcome and add functions

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import os
+import time
+
+library_catalog = {}
 
 # Create welcome function
 def welcome():
-    pass
+    input("Welcome to our library, to show the list of our service press enter...")
+    print("Getting the list, Please wait...")
+    time.sleep(2)
+    print("Please select a number from the list below: ")
+    print("1- Add a book")
+    print("2- Check out")
+    print("3- Check in")
+    print("4- List of the books")
+    print("5- Exit")
 
 # Create function to clear the screen
 def clear():
@@ -10,7 +21,23 @@ def clear():
 
 # Create add function
 def add():
-    pass
+    while True:
+        print("------------------------------------------------------------")
+        isba = input("Please enter the ISBA for the book you want to add: ")
+        if not isba.isdigit():
+            print("\nInvalid input, That is not a number Please enter number between (1 - 5)")
+        elif  (1 > int(isba)) or (int(isba) > 5):
+            print("\nInvalid input, Please enter number between (1 - 5)")
+        elif isba in library_catalog:
+            print("\nSorry this ISBA is already used for another book, enter another ISBA")
+        else:
+            break
+    title = input("Enter the book's title name: ")
+    author = input("Enter th book's author name: ")
+    library_catalog[isba] = {"title": title, "author": author, "available": True }
+    if input("Do you want to add another book? (Y/N) ").lower() == "y":
+        add()
+
 
 # Create check out function
 def check_out():
